8ce27c0a5b772dd3bba8d692e8551fd3bd622e65
acmyu <amycmyu@gmail.com>
2025-07-16 22:11:54 -0400

    image preview

--- app/components/editor/AssetsPanel/Character/Modal.tsx
+++ app/components/editor/AssetsPanel/Character/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Modal from "react-modal";
 import { ErrorMessage, Field, Formik } from "formik";
 import * as Yup from "yup";
@@ -20,6 +20,8 @@ const CharacterModal: React.FC<CustomModalProps> = ({
 }) => {
   // Ref for the file Input element.
   const fileInputRef = useRef<HTMLInputElement>(null);
+  
+  const [preview, setPreview] = useState(null);
 
   const handleFileInputClick = () => {
     fileInputRef.current?.click();
@@ -33,25 +35,23 @@ const CharacterModal: React.FC<CustomModalProps> = ({
     <Formik
       initialValues={{
         requestType: "",
-        subject: "",
-        helps: "",
         file: null as File | null,
       }}
       // In summary, Yup is responsible for the schema-based validation, while Formik handles form management and state. Combining Yup with Formik provides a robust solution for building forms with client-side validation.
 
       validationSchema={Yup.object({
         requestType: Yup.string().required("Required"),
-        subject: Yup.string()
-          .required("Required")
-          .min(3, "Must be at least 3 characters"),
-        helps: Yup.string()
-          .required("Required")
-          .min(6, "Must be at least 6 characters"),
         file: Yup.mixed()
           .required("Required")
+          .test("numImages", "Add more images", (value) => {
+            // Size less than or equal to 1,000,000 bytes (1MB).
+            return value.length >= 2;
+            
+          })
           .test("fileSize", "File size is too large", (value) => {
             // Size less than or equal to 1,000,000 bytes (1MB).
-            return value instanceof File && value.size <= 1000000;
+            return Array.from(value).every(val => val instanceof File && val.size <= 1000000);
+            
           }),
       })}
       onSubmit={(values, { setSubmitting }) => {
@@ -79,13 +79,13 @@ const CharacterModal: React.FC<CustomModalProps> = ({
           style={modalStyle}
         >
           <p className="w-full pb-8 text-sm text-gray-800 fon">
-            Our Support Team is ready to help!
+            Character Editor
           </p>
 
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="block py-2">
               <label className="text-sm font-semibold text-gray-500 ">
-                Request Type
+                Character Type
               </label>
 
               <Field
@@ -95,10 +95,9 @@ const CharacterModal: React.FC<CustomModalProps> = ({
                 value={values.requestType}
                 className="w-full px-4 py-2 border border-gray-300 rounded "
               >
-                <option value="">Select Request Type</option>
-                <option value="General">General</option>
-                <option value="Technical">Technical</option>
-                <option value="Financial">Financial</option>
+                <option value="">Select Character Type</option>
+                <option value="Humanoid">Humanoid</option>
+                <option value="Animal">Animal</option>
                 <option value="Other">Other</option>
               </Field>
 
@@ -110,53 +109,10 @@ const CharacterModal: React.FC<CustomModalProps> = ({
             </div>
 
             <div className="block py-2">
-              <label className="py-2 text-sm font-semibold text-gray-500">
-                Subject
-              </label>
-
-              <Field
-                type="text"
-                name="subject"
-                placeholder="Subject"
-                onChange={handleChange}
-                value={values.subject}
-                className="w-full px-4 py-2 border border-gray-300 rounded"
-              />
-
-              <ErrorMessage
-                name="subject"
-                component="div"
-                className="py-1 text-sm italic font-semibold text-red-500"
-              />
-            </div>
-
-            <div className="block py-2">
-              <label className="text-sm font-semibold text-gray-500">
-                How we can help?
-              </label>
-              <Field
-                as="textarea"
-                name="helps"
-                placeholder="How we can help?"
-                onChange={handleChange}
-                value={values.helps}
-                className="w-full px-4 py-2 border border-gray-300 rounded"
-                rows={6}
-              />
-
-              <ErrorMessage
-                name="helps"
-                component="div"
-                className="py-1 text-sm italic font-semibold text-red-500"
-              />
-            </div>
-
-            <div className="block py-2">
               <div className="mb-3">
                 <label className="text-sm font-semibold text-gray-500 ">
-                  Attachments
+                  Images
                 </label>
-                <span className="ml-2 text-sm text-gray-500">Optional </span>
               </div>
 
               <div className="block w-full py-8 text-sm text-center border-2 border-dashed text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:text-sm file:font-semibold file:bg-violet-50 file:text-gray-500 hover:file:bg-violet-100">
@@ -167,7 +123,7 @@ const CharacterModal: React.FC<CustomModalProps> = ({
                 >
                   <span className="flex items-center justify-center w-full h-full">
                     <p className="text-sm font-semibold">
-                      Add up to 4 screenshot
+                      Add at least 2 images of the character
                     </p>
                   </span>
                 </label>
@@ -175,12 +131,41 @@ const CharacterModal: React.FC<CustomModalProps> = ({
                   type="file"
                   className="hidden"
                   name="file"
+                  multiple
                   ref={fileInputRef}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    const selectedValue = e.target.files && e.target.files[0];
+                    var selectedValue = e.target.files;
+                    if (values.file) {
+                      selectedValue = [...values.file, ...selectedValue];
+                    }
                     setFieldValue("file", selectedValue);
                   }}
                 />
+                
+                {values.file && (
+                  <div>
+                    {
+                      Array.from(values.file).map(
+                        (image, index) =>    
+                        <img
+                          key={index}
+                          alt="preview"
+                          width={"250px"}
+                          src={URL.createObjectURL(image)}
+                          style={{display: 'inline'}}
+                          className="block p-2"
+                        />
+                      )
+                    }
+                    <div className="block py-2">
+                    <button onClick={() => {
+                      setPreview(null);
+                      setFieldValue("file", null);
+                    }}>Clear</button>
+                    </div>
+                  </div>
+                )}
+                
               </div>
               <ErrorMessage
                 name="file"
@@ -204,7 +189,7 @@ const CharacterModal: React.FC<CustomModalProps> = ({
                 disabled={isSubmitting}
                 className="px-4 py-2 mt-4 font-medium text-white bg-indigo-400 rounded-md "
               >
-                Send
+                Save
               </button>
             </div>
           </form>
